740896c3435df946e22fbc6b8d694219b99bca07
LauriKatajisto <lauri.katajisto@gmail.com>
2018-06-22 14:07:34 +0300

    Added working app routing.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
-import Menu from "./components/Menu";
-import Footer from "./components/Footer";
+import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <Menu />
-            <br />
-            <div className="ui container">
-              <h2>Rapid Development Platform</h2>
-            </div>
-          <Footer />
-        </div>
+        <Routes />
     );
   }
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Links from "./components/Links";
-import App from "./App";
+import Frontpage from "./components/Frontpage";
 
 export default class Routes extends React.PureComponent {
   render() {
     return(
       <Router>
         <Switch>
-          <Route path="/" component={App} exact />
+          <Route path="/" component={Frontpage} exact />
           <Route path="/Links" component={Links} exact />
         </Switch>
       </Router>
