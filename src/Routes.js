0cc1aa84c3bcdfb7519d786a49cdad352b127223
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-06-25 22:39:08 +0300

    Fixed routing for Table-page. Made table-page first layout.

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Links from "./components/Links";
 import Frontpage from "./components/Frontpage";
+import Table from "./components/Table";
 
 export default class Routes extends React.PureComponent {
   render() {
@@ -10,6 +11,7 @@ export default class Routes extends React.PureComponent {
         <Switch>
           <Route path="/" component={Frontpage} exact />
           <Route path="/Links" component={Links} exact />
+          <Route path="/Table" component={Table} exact />
         </Switch>
       </Router>
     );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,93 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import Layout from "./Layout";
-import { Header } from "semantic-ui-react";
+import _ from "lodash";
+import { Header, Table } from "semantic-ui-react";
 
-class Table extends PureComponent {
+const tableData = [
+  { id: "1", product: "Product 1", price: 10, category: "Industry" },
+  { id: "2", product: "Product 2", price: 80, category: "Industry" },
+  { id: "3", product: "Product 3", price: 20, category: "Coal" },
+  { id: "4", product: "Product 4", price: 10, category: "Industry" }
+];
+
+export default class TableExampleSortable extends Component {
+  state = {
+    column: null,
+    data: tableData,
+    direction: null
+  };
+
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state;
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: "ascending"
+      });
+
+      return;
+    }
+
+    this.setState({
+      data: data.reverse(),
+      direction: direction === "ascending" ? "descending" : "ascending"
+    });
+  };
+
   render() {
+    const { column, data, direction } = this.state;
     return (
       <Layout>
-        <div>
-          <Header as="h2">Table from JSON data</Header>
-        </div>
+        <Header as="h2">Table from JSON data</Header>
+        <Table sortable selectable celled fixed compact="very" color="purple">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === "id" ? direction : null}
+                onClick={this.handleSort("id")}
+              >
+                Id
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "product" ? direction : null}
+                onClick={this.handleSort("product")}
+              >
+                Product
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "price" ? direction : null}
+                onClick={this.handleSort("price")}
+              >
+                Price
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "category" ? direction : null}
+                onClick={this.handleSort("category")}
+              >
+                Category
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {_.map(data, ({ product, price, id, category }) => (
+              <Table.Row key={id}>
+                <Table.Cell>{id}</Table.Cell>
+                <Table.Cell>{product}</Table.Cell>
+                <Table.Cell>{price}</Table.Cell>
+                <Table.Cell>{category}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+          <Table.Footer>
+            <Table.HeaderCell>Row count?</Table.HeaderCell>
+            <Table.HeaderCell />
+            <Table.HeaderCell />
+            <Table.HeaderCell />
+          </Table.Footer>
+        </Table>
       </Layout>
     );
   }
 }
-
-export default Table;
