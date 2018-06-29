3f5aff3b9b455a07a85a7b74617b3c724d2144d5
LauriKatajisto <lauri.katajisto@gmail.com>
2018-06-29 22:43:07 +0300

    Added context to use app state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
 import Routes from "./Routes";
+import { Context } from './context';
 
 class App extends Component {
+  constructor(p) {
+    super(p);
+    this.state = {
+      searchQuery: '',
+      updateSearchQuery: this.updateSearchQuery
+    };
+  }
+
+  updateSearchQuery = (searchQuery) => {
+    this.setState({
+      searchQuery,
+    })
+  }
+
   render() {
-    return <Routes />;
+    return (
+      <Context.Provider value={this.state}>
+        <Routes />
+      </Context.Provider>
+    );
   }
 }
 
