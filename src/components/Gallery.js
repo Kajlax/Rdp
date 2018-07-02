2792c9ebd2ac7108363edb63827bfc84e0ba54bd
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-07-02 14:04:01 +0300

    Gallery-component dimmer functionality started, but not yet working.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./Layout";
-import { Grid, Image, Header } from "semantic-ui-react";
+import { Dimmer, Grid, Image, Header } from "semantic-ui-react";
 import JSONdata from "../data/GalleryData.json";
 
-export default class Gallery extends React.PureComponent {
+export default class Gallery extends Component {
+  state = {};
+
   renderGalleryArray = () => {
     return JSONdata.map(item => {
       return (
         <Grid.Column key={item.id}>
-          <Image rounded src={item.imageUrl} />
+          <Image rounded src={item.imageUrl} onClick={this.handleOpen} />
         </Grid.Column>
       );
     });
+  };
+
+  handleOpen = () => this.setState({ active: true });
+  handleClose = () => this.setState({ active: false });
+
+  showImage = () => {
+    const { active } = this.state;
+    return (
+      <Dimmer active={active} onClickOutside={this.handleClose} page>
+        <Image rounded src={this.imageUrl} />
+        <Header as="h2" inverted>
+          {this.id}, {this.imageCategory}
+        </Header>
+      </Dimmer>
+    );
   };
 
   render() {
