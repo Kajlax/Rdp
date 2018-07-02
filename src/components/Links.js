82548b350ff08f9c8b08ccd4c1df3f6d4d6861aa
LauriKatajisto <lauri.katajisto@gmail.com>
2018-07-02 19:22:29 +0300

    Links cleanup
    - get categories from file
    - use one function to render links

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "./Layout";
+import _ from "lodash";
 import { Grid, Header, List } from "semantic-ui-react";
 import JSONdata from "../data/LinksData.json";
 
+const categories = _.uniqBy(JSONdata, 'linkCategory');
+
 export default class Links extends React.PureComponent {
-  renderLinksArrayTools = () => {
-    return JSONdata.filter(item => item.linkCategory === "Tools").map(item => {
+  renderLinksArray = (section) => {
+    return JSONdata.filter(item => item.linkCategory === section).map(item => {
       return (
         <List.Item key={item.linkName}>
           <List.Icon name="caret right" />
@@ -16,51 +19,18 @@ export default class Links extends React.PureComponent {
       );
     });
   };
-
-  renderLinksArrayProducts = () => {
-    return JSONdata.filter(item => item.linkCategory === "Products").map(
-      item => {
-        return (
-          <List.Item key={item.linkName}>
-            <List.Icon name="caret right" />
-            <List.Content>
-              <a href={item.linkUrl}>{item.linkName}</a>
-            </List.Content>
-          </List.Item>
-        );
-      }
-    );
-  };
-
-  renderLinksArraySupport = () => {
-    return JSONdata.filter(item => item.linkCategory === "Support").map(
-      item => {
-        return (
-          <List.Item key={item.linkName}>
-            <List.Icon name="caret right" />
-            <List.Content>
-              <a href={item.linkUrl}>{item.linkName}</a>
-            </List.Content>
-          </List.Item>
-        );
-      }
-    );
-  };
-
-  renderLinksArrayWebsites = () => {
-    return JSONdata.filter(item => item.linkCategory === "Websites").map(
-      item => {
-        return (
-          <List.Item key={item.linkName}>
-            <List.Icon name="caret right" />
-            <List.Content>
-              <a href={item.linkUrl}>{item.linkName}</a>
-            </List.Content>
-          </List.Item>
-        );
-      }
-    );
-  };
+  renderColumns = () => {
+    return categories.map(col => {
+      return (
+        <Grid.Column key={col.linkCategory}>
+          <Header as="h4" dividing>
+            {col.linkCategory}
+          </Header>
+          <List>{this.renderLinksArray(col.linkCategory)}</List>
+        </Grid.Column>
+      );
+    });
+  }
 
   render() {
     return (
@@ -68,30 +38,9 @@ export default class Links extends React.PureComponent {
         <div>
           <Header as="h2">Links</Header>
           <Grid columns={3} stackable>
-            <Grid.Column>
-              <Header as="h4" dividing>
-                Tools
-              </Header>
-              <List>{this.renderLinksArrayTools()}</List>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as="h4" dividing>
-                Products
-              </Header>
-              <List>{this.renderLinksArrayProducts()}</List>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as="h4" dividing>
-                Support
-              </Header>
-              <List>{this.renderLinksArraySupport()}</List>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as="h4" dividing>
-                Websites
-              </Header>
-              <List>{this.renderLinksArrayWebsites()}</List>
-            </Grid.Column>
+            {
+              this.renderColumns()
+            }
           </Grid>
         </div>
       </Layout>
