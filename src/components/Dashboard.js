77817d534b0c8dae7bb817b6e951c148858d871c
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-07-03 14:51:20 +0300

    Added chart.js dependency to package.json. Dashboard alpha version done. Chart.js implemented.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,38 @@ import React from "react";
 import Layout from "./Layout";
 import { Grid, Header, Statistic } from "semantic-ui-react";
 import JSONdata from "../data/DashboardData.json";
+import { Bar } from "react-chartjs-2";
+
+const DashboardBarChart = () => {
+  const data = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ],
+    datasets: [
+      {
+        label: "Monthly users",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56]
+      }
+    ]
+  };
+  return <Bar data={data} />;
+};
 
 export default class Dashboard extends React.PureComponent {
   renderDashboardStatistics = () => {
@@ -23,8 +55,9 @@ export default class Dashboard extends React.PureComponent {
           <Grid.Row>
             <Grid.Column width={16}>
               <Header as="h4" dividing>
-                Monthly users (Chart.js bar chart)
+                Monthly users
               </Header>
+              <DashboardBarChart />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
