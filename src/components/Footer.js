e625cd84843a599dfd8245306e963d5f55b8493f
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-10-12 12:57:55 +0300

    Tweaked footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,38 @@
 import React from "react";
-import { Header, Segment } from "semantic-ui-react";
+import { Grid, Header, Icon, Segment } from "semantic-ui-react";
 
 const segmentStyle = {
   position: "fixed",
   left: 0,
   bottom: 0,
-  height: 80,
   width: "100%",
   borderRadius: 0
 };
 
-export default class Footer extends React.PureComponent {
+class Footer extends React.PureComponent {
   render() {
     return (
       <Segment inverted style={segmentStyle}>
         <div className="ui center aligned container">
-          <Header as="h5" inverted>
-            &copy; Rapid Development Platform, 2018.
-          </Header>
-          <a href="https://www.facebook.com/">
-            <i className="facebook square icon big" />
-          </a>
-          <a href="https://twitter.com/">
-            <i className="twitter square icon big" />
-          </a>
+          <Grid stackable columns={2}>
+            <Grid.Row>
+              <Grid.Column>
+                <Header as="h5" inverted>
+                  Rapid Development Platform, 2018
+                  <br /> info@rdp.fi
+                </Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Icon link name="facebook square" size="large" />
+                <Icon link name="instagram" size="large" />
+                <Icon link name="twitter" size="large" />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       </Segment>
     );
   }
 }
+
+export default Footer;
